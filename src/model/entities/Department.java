47f384038f10898744b624ba0d5cb08c8d9c3983
michelXD <michelXD@gmail.com>
2021-10-28 13:45:10 -0300

    Department service

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -12,13 +12,20 @@ public class Department {
 		this.id = id;
 		this.name = name;
 	}
+		
 	public Integer getId() {
 		return id;
 	}
-	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
